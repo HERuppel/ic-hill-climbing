e4c255e027acf4e8a56ed3d301eb9724ac963276
HERuppel <heruppel2@gmail.com>
2022-08-24 15:01:24 -0300

    prop name change

--- index.py
+++ index.py
@@ -21,13 +21,12 @@ def loadData():
 
   return data[:, :-1], data[:, -1]
 
-def hillClimbing(X_train, y_train, X_test, y_test, hiddenLayerNodes):
+def hillClimbing(X_train, y_train, X_test, y_test, hidden_layer_sizes):
   clf = MLPClassifier(
     early_stopping=True, 
-    verbose=True, 
     max_iter=10000, 
     alpha=1e-5, 
-    hidden_layer_sizes=hiddenLayerNodes, 
+    hidden_layer_sizes=hidden_layer_sizes, 
     tol=1e-8, 
     random_state=1,
     learning_rate_init=.01)
@@ -41,12 +40,12 @@ def hillClimbing(X_train, y_train, X_test, y_test, hiddenLayerNodes):
 
 
 
-X, y = loadData()
-#X, y = make_classification(n_samples=1000, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=5)
+#X, y = loadData()
+X, y = make_classification(n_samples=1000, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
 #Primeira camada 2 - 15
-scores1, accuracy1 = hillClimbing(X_train, y_train, X_test, y_test, (randint(2, 15),randint(1, 10),))
+scores1, accuracy1 = hillClimbing(X_train, y_train, X_test, y_test, (randint(2, 15),randint(1, 10)))
 
 #Segunda camada 0 - 10 *(1 - 10)
 #scores2, accuracy2 = hillClimbing(X_train, y_train, X_test, y_test, (randint(1, 10),))
