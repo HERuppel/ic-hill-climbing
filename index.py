faf4e23fc13d4a9c8a2803ecb39fa0ef4c3c861f
HERuppel <heruppel2@gmail.com>
2022-08-24 14:47:33 -0300

    semeion data

--- index.py
+++ index.py
@@ -5,13 +5,16 @@ from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
+from sklearn.datasets import make_classification
+from numpy.random import seed
+
+seed(1)
 
 def loadData():
   dataframe = pd.read_csv(
     filepath_or_buffer=os.path.join(
-      os.path.dirname(__file__), 'sets\dataR2.csv'),
-      header=None,
-      delimiter=","
+      os.path.dirname(__file__), 'sets\semeion.data'),
+      delimiter=r"\s+"
   )
 
   data = dataframe.values
@@ -39,6 +42,7 @@ def hillClimbing(X_train, y_train, X_test, y_test, hiddenLayerNodes):
 
 
 X, y = loadData()
+#X, y = make_classification(n_samples=1000, random_state=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=5)
 
 #Primeira camada 2 - 15
