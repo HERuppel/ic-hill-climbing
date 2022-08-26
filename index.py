17d1c3ea86aeb3277b1eeedc9d90ecad8f587c33
Renato Ruppel <heruppel2@gmail.com>
2022-08-26 00:51:34 -0300

    minor tweaks

--- index.py
+++ index.py
@@ -1,85 +1,120 @@
 import os
+import time
+import warnings
 from random import randint
-import pandas as pd
+import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
-from sklearn.datasets import make_classification
-from numpy.random import seed
+import pandas as pd
+from matplotlib import pyplot
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
-seed(1)
+warnings.filterwarnings('ignore')
 
 def loadData():
   dataframe = pd.read_csv(
     filepath_or_buffer=os.path.join(
-      os.path.dirname(__file__), 'sets\semeion.data'),
-      delimiter=r"\s+"
+      os.path.dirname(__file__), 'sets\dataR2.csv'),
+      header=None,
+      delimiter=","
   )
 
   data = dataframe.values
 
   return data[:, :-1], data[:, -1]
 
-def evaluate_predictions(y_test, yhat):
-	return metrics.accuracy_score(y_test, yhat)
+def evalPrediction(Y, candidate):
+  return metrics.accuracy_score(Y, candidate)
 
-def random_predictions(n_examples):
-	return [randint(0, 1) for _ in range(n_examples)]
+def randPredictions(numExamples):
+  return [randint(0, 1) for _ in range(numExamples)]
 
-def modify_predictions(current, n_changes=1):
-	updated = current.copy()
-	for i in range(n_changes):
-		ix = randint(0, len(updated)-1)
-		updated[ix] = 1 - updated[ix]
-	return updated
+def modifyPredictions(currentSolution, numChanges=1):
+  updated = currentSolution.copy()
+  for _ in range(numChanges):
+    ix = randint(0, len(updated) - 1)
+    updated[ix] = 1 - updated[ix]
+  return updated
 
-def hillClimbing(X_test, y_test, maxIterations):
-	scores = list()
+def classification_hillclimbing(X_train, X_test, y_train, y_test):
+    bestAccuracy = -np.inf
+    bestI = -1
+    bestJ = -1
+    scores = list()
+    solution = randPredictions(X.shape[0])
+    score = evalPrediction(Y, solution)
+    specificities = list()
+    sensitivities = list()
+    trainScores = list()
+    testScores = list()
 
-	solution = random_predictions(X_test.shape[0])
-	score = evaluate_predictions(y_test, solution)
-	scores.append(score)
+    for i in range(2, 15): 
+      print("Testando {}".format("." * i))
+      for j in range(0, 10):
+        hiddenLayerSizes = (i) if j < 2 else (i, j)
+        clf = MLPClassifier(solver='lbfgs', max_iter=30000, alpha=1e-5, hidden_layer_sizes=hiddenLayerSizes, random_state=int(time.time()))
+        clf.fit(X_train, y_train)
 
-	for i in range(maxIterations):
-		scores.append(score)
-		if score == 1.0:
-			break
-		candidate = modify_predictions(solution)
-		value = evaluate_predictions(y_test, candidate)
-		if value >= score:
-			solution, score = candidate, value
-	return solution, scores
+        yPred = clf.predict(X_test)
+        
+        tn, fp, fn, tp = confusion_matrix(y_test, yPred).ravel()
+        specificity = tn / (tn+fp)
+        specificities.append(specificity)
 
-def classification(X_train, y_train, X_test, y_test, hidden_layer_sizes):
-  clf = MLPClassifier(
-    solver='lbfgs',
-    max_iter=10000, 
-    alpha=1e-5, 
-    hidden_layer_sizes=hidden_layer_sizes, 
-    random_state=1)
-  clf.fit(X_train, y_train)
+        sensitivity = tp / (tp + fn)
+        sensitivities.append(sensitivity)
 
-  Ypred = clf.predict(X_test)
-  accuracy = metrics.accuracy_score(y_test, Ypred)
-  return round(accuracy * 100, 2)
+        trainPred = clf.predict(X_train)
+        trainAcc = metrics.accuracy_score(y_train, trainPred)
+        trainScores.append(trainAcc)
+
+        testPred = clf.predict(X_test)
+        testAcc = metrics.accuracy_score(y_test, testPred)
+        testScores.append(testAcc)
+
+        candidate = modifyPredictions(solution)
+        value = evalPrediction(Y, candidate)
+        if value >= score:
+          solution, score = candidate, value
+          bestAccuracy = value
+          bestI = i
+          bestJ = j
+        scores.append(score)
+    return bestAccuracy, bestI, bestJ, scores, specificities, sensitivities, trainScores, testScores
 
 
+X, Y = loadData()
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=int(time.time()))
+bestAccuracy, bestI, bestJ, scores, specificities, sensitivities, trainScores, testScores = classification_hillclimbing(X_train, X_test, y_train, y_test)
 
-#X, y = loadData()
-X, y = make_classification(n_samples=1000, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
-test = hillClimbing(X_test, y_test, 1000)
-score = classification(X_train, y_train, X_test, y_test, (randint(2, 15),randint(1, 10)))
+print("Melhor acurácia: {}%".format(round(bestAccuracy * 100, 2)))
+print("Nós do melhor resultado - 1ª Camada: {} | 2ª Camada: {} ".format(bestI, bestJ))
 
-print(score)
-#Primeira camada 2 - 15
-#scores1, accuracy1 = hillClimbing(X_train, y_train, X_test, y_test, (randint(2, 15),randint(1, 10)))
 
-#Segunda camada 0 - 10 *(1 - 10)
-#scores2, accuracy2 = hillClimbing(X_train, y_train, X_test, y_test, (randint(1, 10),))
+pyplot.figure(num='Acurácia')
+pyplot.title('Acurácia a cada iteração')
+pyplot.xlabel('Iterações')
+pyplot.ylabel('Acurácia')
+pyplot.plot(scores)
+pyplot.show()
 
-#pyplot.plot(test[1])
-#pyplot.show()
-# print(accuracy2)
-# pyplot.plot(scores2)
-# pyplot.show()
+pyplot.figure(num='Especificidade')
+pyplot.title('Especificidade a cada iteração')
+pyplot.xlabel('Iterações')
+pyplot.ylabel('Especificidade')
+pyplot.plot(specificities)
+pyplot.show()
+
+pyplot.figure(num='Sensibilidade')
+pyplot.title('Sensibilidade a cada iteração')
+pyplot.xlabel('Iterações')
+pyplot.ylabel('Sensiblididade')
+pyplot.plot(sensitivities)
+pyplot.show()
+
+pyplot.figure(num='Overfitting')
+pyplot.plot(trainScores, '-o', label='Treino')
+pyplot.plot(testScores, '-o', label='Teste')
+pyplot.legend()
+pyplot.show()
